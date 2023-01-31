10c4e84ceddda88cde5a9fe213fb37fb1a605605
robguet <robguet@hotmail.com>
2023-01-30 22:50:18 -0600

    cron job logic

--- cron-jobs/cron.js
+++ cron-jobs/cron.js
@@ -4,7 +4,7 @@ const { updateMonthlyCharges } = require('../helper/db-helper');
 
 module.exports = () => {
 
-    cron.schedule('12 * * * * *', () => {
+    cron.schedule('0 0 0 * * *', () => {
         console.log('running at 12AM');
         updateMonthlyCharges()
     });
